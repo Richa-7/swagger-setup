1210aee53d426952b9a71d29688a9e278675529f
Richa-7 <richachhabra07@gmail.com>
2020-08-13 22:39:46 +0530

    Swagger customization and annotations

--- src/main/java/com/richa/swagger/setup/SwaggerConfig.java
+++ src/main/java/com/richa/swagger/setup/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.richa.swagger.setup;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +23,26 @@ public class SwaggerConfig {
           .select()
           .apis(RequestHandlerSelectors.basePackage("com.richa.swagger.setup"))              
           .paths(PathSelectors.any())
-          .build();                                           
+          .build()
+          .apiInfo(apiDetails());   
+        
+        //select method to get the builder object 
+        //After setting the changes build is used
+        //paths => for restricting api paths
+        // api base package restricts from other packages for swagger
+        //If I remove .api then Spring MVC mappings can also be seen in swagger
+    }
+    
+    public ApiInfo apiDetails() {
+    	return new ApiInfo(
+    			"Swagger Setup and Customization",
+    			"Sample api for learning",
+    			"1.0",
+    			"Free to use",
+    			new Contact("Richa Chhabra", "https://github.com/", "richachhabra07@gmail.com"),
+    			"API License",
+    			"https://github.com/",
+    			Collections.emptyList());
+    			
     }
 }
--- src/main/java/com/richa/swagger/setup/WelcomeController.java
+++ src/main/java/com/richa/swagger/setup/WelcomeController.java
@@ -4,12 +4,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api")
 public class WelcomeController {
 
 	@GetMapping("/welcome")
+	@ApiOperation(value="welcome to swagger setup",
+		notes= "Give information about what API does",
+		response=String.class)
 	public String hello() {
 		return "Welcome";
 	}
+	
+	
+	/*
+	 * @ApiParam when we want to pass parameters to API add description in value
+	 * about what that parameter is used for
+	 * 
+	 * @ApiModel and @ApiOperation used on DTO class and attributes respectively
+	 */
 }
